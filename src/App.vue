715b85d094a32bda1cdbcd2a21f2280cd6a217ae
moka12579 <71615766+moka12579@users.noreply.github.com>
2022-10-24 09:33:11 +0800

    解决部分问题

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,17 @@
 </template>
 
 <style lang="scss">
-
+@font-face {
+  font-family: 'iconfont';  /* Project id 3710512 */
+  src: url('//at.alicdn.com/t/c/font_3710512_jsmml0nxzpd.woff2?t=1666077665720') format('woff2'),
+  url('//at.alicdn.com/t/c/font_3710512_jsmml0nxzpd.woff?t=1666077665720') format('woff'),
+  url('//at.alicdn.com/t/c/font_3710512_jsmml0nxzpd.ttf?t=1666077665720') format('truetype');
+}
+.iconfont{
+  font-family:"iconfont" !important;
+  font-size:16px;font-style:normal;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-stroke-width: 0.2px;
+  -moz-osx-font-smoothing: grayscale;
+}
 </style>

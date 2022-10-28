0a2ecc2fa0801ac1f109fb2b8e18489dc0973ef4
moka12579 <71615766+moka12579@users.noreply.github.com>
2022-10-28 09:22:35 +0800

    解决部分问题

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-const {merge} = require('webpack-merge')
-const tsImportPluginFactory = require('ts-import-plugin')
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -16,27 +14,4 @@ module.exports = defineConfig({
       }
     }
   },
-  chainWebpack: config => {
-    config.module
-        .rule('ts')
-        .use('ts-loader')
-        .tap(options => {
-          options = merge(options, {
-            transpileOnly: true,
-            getCustomTransformers: () => ({
-              before: [
-                tsImportPluginFactory({
-                  libraryName: 'vant',
-                  libraryDirectory: 'es',
-                  style: true
-                })
-              ]
-            }),
-            compilerOptions: {
-              module: 'es2015'
-            }
-          });
-          return options;
-        });
-  }
 })
